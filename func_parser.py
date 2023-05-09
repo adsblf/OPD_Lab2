fdad88fdb585f81383d6689d9402a12fdac9c359
adsblf <125277239+adsblf@users.noreply.github.com>
2023-05-09 10:25:06 +0600

    Update func_parser.py

--- func_parser.py
+++ func_parser.py
@@ -4,7 +4,7 @@ def parser():
     string = ['Стэтэм', 'Чак Норис', 'Сталоне', 'Рокки', 'Деньги', 'Спорт', 'Учеба', 'Бизнес', 'Сталин', 'Ленин',
               'Бредд Пит', 'МакКонахи', 'Генри Форд', 'Джим Керри', 'Успех', ' ', ' ', ' ', 'музыка', 'Медитация',
               'диета', 'здоровый образ жизни', 'победа', 'бокс', 'саморазвитие', 'личностный рост', 'инвестиции']
-    google_crawler = GoogleImageCrawler(storage={'root_dir': 'C:/Users/main/Desktop/OPD_2/pic'})
+    google_crawler = GoogleImageCrawler(storage={'root_dir': 'C:/Users/main/Desktop/OPD_Lab2/pic'})
     google_crawler.crawl(keyword = "Мотивирующая цитата " + string[random.randint(0, len(string))] + " на картинке",
                      max_num=10,
                      file_idx_offset='auto')
